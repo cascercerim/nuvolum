9de2db161aaf022ab80492a4c35efef310e7482a
Maria <cascercerim@gmail.com>
2020-06-06 00:40:54 -0400

    nav and footer responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Navigation from './components/navigation';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import PageRenderer from './page-renderer';
 import Carousel from './components/carousel';
 import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -13,9 +13,6 @@ function App() {
       <div className="App">
         <Navigation />
         <Switch>
-          <Route path="/:page" component={PageRenderer} />
-          <Route path="/" render={() => <Redirect to="/home" />} />
-          <Route component={() => 404} />
         </Switch>
         <Carousel />
         <Footer />
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,44 @@ import React from 'react'
 
 export default function footer() {
     return (
-        <div className="footer">
-
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/* Column1 */}
+                    <div className="col">
+                        <h5>Nuvolum Test</h5>
+                        <ul className="list-unstyled">
+                            <li>123-456-7890</li>
+                            <li>North Pole, Santa's House</li>
+                            <li>123 Candy Cane Lane</li>
+                        </ul>
+                    </div>
+                    {/* Column2 */}
+                    <div className="col">
+                        <h5>STUFF</h5>
+                        <ul className="list-unstyled">
+                            <li>Careers</li>
+                            <li>Services</li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+                    {/* Column3 */}
+                    <div className="col">
+                        <h5>ANOTHER COLUMN</h5>
+                        <ul className="list-unstyled">
+                            <li>Cookies</li>
+                            <li>Privacy and Terms</li>
+                            <li>About</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr className="line-break" />
+                <div className="row">
+                    <p className="col-sm">
+                        &copy;{new Date().getFullYear()} NUVOLUM CODE TEST | All rights reserved | Terms Of Service | Privacy
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
